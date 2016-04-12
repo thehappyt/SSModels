a4b36426df6b391285bf79d6eb2994943e80dd30
Adam Thompson <thompson.adams+git@gmail.com>
2016-04-11 20:29:22 -0700

    Attempt to fix error with resize after initialization.

--- gsssm0.1.js
+++ gsssm0.1.js
@@ -3,14 +3,18 @@
     
     var canvases = [];
     
+    var high = Number(window.innerHeight) - 57;                     // 36 for status bar, 21 for title
+    var wide = Number(window.innerWidth) - 20                       // 20 for good measure
+    var cvsdim = Math.min(high,(wide-200)/2.0);
+    
     var cvs2overwrap = $("<div/>").addClass("SSM_gswrap");
-    var cvs2 = canvas({ resizable:false });
+    var cvs2 = canvas({ resizable:false, width:cvsdim, height: cvsdim });
     canvas.container.prepend(cvs2overwrap);
     cvs2overwrap.append(cvs2.title_anchor).append(cvs2.wrapper).append(cvs2.caption_anchor);
     cvs2.overwrap = cvs2overwrap;
     
     var cvs1overwrap = $("<div/>").addClass("SSM_gswrap");
-    var cvs1 = canvas({ resizable:false });
+    var cvs1 = canvas({ resizable:false, width:cvsdim, height: cvsdim });
     canvas.container.prepend(cvs1overwrap);
     cvs1overwrap.append(cvs1.title_anchor).append(cvs1.wrapper).append(cvs1.caption_anchor);
     cvs1.overwrap = cvs1overwrap;
@@ -20,11 +24,6 @@
     
     //this.canvas.wrapper.css({float:"left"});
     
-    var high = Number(window.innerHeight) - 57;                     // 36 for status bar, 21 for title
-    var wide = Number(window.innerWidth) - 20                       // 20 for good measure
-    var cvsdim = Math.min(high,(wide-200)/2.0);
-    
-    cvs1.width = cvs1.height = cvs2.width = cvs2.height = cvsdim;   // Set canvas dimensions.
     sphere({ canvas: cvs1, visible: false });                       // Activate cvs1.
     sphere({ canvas: cvs2, visible: false });                       // Activate cvs2.
     
